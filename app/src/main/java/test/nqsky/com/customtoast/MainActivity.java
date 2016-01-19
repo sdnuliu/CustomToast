a3f09f8f526ff79337a5e8e8f58f56512938f2ea
liuteng <sdnuliu@qq.com>
2016-01-19 13:44:49 +0800

    add CLog,test log print

--- app/src/main/java/test/nqsky/com/customtoast/MainActivity.java
+++ app/src/main/java/test/nqsky/com/customtoast/MainActivity.java
@@ -3,7 +3,10 @@ package test.nqsky.com.customtoast;
 import android.os.Bundle;
 import android.view.View;
 
+import test.nqsky.com.customtoast.clog.CLog;
+
 public class MainActivity extends BaseActivity {
+    public static final String tag= "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,10 @@ public class MainActivity extends BaseActivity {
                 createToastWindow("按钮被点击了！！！");
             }
         });
+        CLog.setLogLevel(CLog.LEVEL_VERBOSE);
+        CLog.v(tag,"v测试");
+        CLog.v(tag,"v测试1,%s,%s","v测试2","v测试3");
+        CLog.d(tag,"d测试");
+        CLog.d(tag,"d测试1,%s,%s","d测试2","d测试3");
     }
 }
